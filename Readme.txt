cd77a37444c2b9d15682960df41e9eeb0b2ea628
yiyisong <susiesong423@163.com>
2022-09-16 11:04:24 +0800

    first commit

---
+++ Readme.txt
@@ -0,0 +1,1 @@
+Author: songyiyi@wondersgroup.com
---
+++ js/main.js
@@ -0,0 +1,217 @@
+(function ($) {
+    "use strict";
+    var nav = $('nav');
+    var navHeight = nav.outerHeight();
+
+    $('.navbar-toggler').on('click', function () {
+        if (!$('#mainNav').hasClass('navbar-reduce')) {
+            $('#mainNav').addClass('navbar-reduce');
+        }
+    })
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
+
+    /**
+     * Function to remove scroll bar during preload
+     */
+    $(window).on("load", function () {
+        setTimeout(function () {
+            $(".no-scroll-preload").css("overflow", "visible");
+        }, 1000);
+        $(".loader-container").fadeOut(2500);
+    });
+
+    /**
+     * Preloader
+     */
+    $(window).on('load', function () {
+        if ($('#preloader').length) {
+            $('#preloader').delay(100).fadeOut('slow', function () {
+                $(this).remove();
+            });
+        }
+    });
+
+    /**
+     * Back to top button
+     */
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    /**
+     * ScrollTop
+     */
+    $('.scrolltop-mf').on("click", function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    /**
+     * Scrolling nav
+     */
+    $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - navHeight + 5)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    /**
+     * Closes responsive menu when a scroll trigger link is clicked
+     */
+    $('.js-scroll').on("click", function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+    /**
+     * Activate scrollspy to add active class to navbar items on scroll
+     */
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: navHeight
+    });
+
+    /**
+     * Navbar Menu Reduce
+     */
+    $(window).trigger('scroll');
+    $(window).on('scroll', function () {
+        var pixels = 50;
+        var top = 1200;
+        if ($(window).scrollTop() > pixels) {
+            $('.navbar-expand-md').addClass('navbar-reduce');
+            $('.navbar-expand-md').removeClass('navbar-trans');
+        } else {
+            $('.navbar-expand-md').addClass('navbar-trans');
+            $('.navbar-expand-md').removeClass('navbar-reduce');
+        }
+        if ($(window).scrollTop() > top) {
+            $('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+        } else {
+            $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+        }
+    });
+
+    /**
+     * Typed
+     */
+    if ($('.text-slider').length == 1) {
+        var typed_strings = $('.text-slider-items').text();
+        var typed = new Typed('.text-slider', {
+            strings: typed_strings.split(','),
+            typeSpeed: 80,
+            loop: true,
+            backDelay: 1100,
+            backSpeed: 30
+        });
+    }
+
+    /**
+     * Skills animation
+     */
+    let skilsContent = select('.skills-content');
+    if (skilsContent) {
+        new Waypoint({
+            element: skilsContent,
+            offset: '80%',
+            handler: function(direction) {
+                let progress = select('.progress .progress-bar', true);
+                progress.forEach((el) => {
+                    el.style.width = el.getAttribute('aria-valuenow') + '%'
+                });
+            }
+        })
+    }
+
+    /**
+     * Porfolio isotope and filter
+     */
+    window.addEventListener('load', () => {
+        let portfolioContainer = select('.portfolio-container');
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+                itemSelector: '.portfolio-item',
+                layoutMode: 'fitRows'
+            });
+
+            let portfolioFilters = select('#portfolio-flters li', true);
+
+            on('click', '#portfolio-flters li', function (e) {
+                e.preventDefault();
+                portfolioFilters.forEach(function (el) {
+                    el.classList.remove('filter-active');
+                });
+                this.classList.add('filter-active');
+
+                portfolioIsotope.arrange({
+                    filter: this.getAttribute('data-filter')
+                });
+            }, true);
+        }
+
+    });
+
+    /**
+     * Initiate portfolio lightbox
+     */
+    const portfolioLightbox = GLightbox({
+        selector: '.portfolio-lightbox'
+    });
+
+    /**
+     * Portfolio details slider
+     */
+    new Swiper('.portfolio-details-slider', {
+        speed: 400,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        }
+    });
+
+
+})(jQuery);
